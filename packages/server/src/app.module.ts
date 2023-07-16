702c2ef5e6a88588b10d699d2a7b2fa42932574a
xbzhang2020 <xbzhang2020@icloud.com>
2023-07-16 13:22:53 +0800

    feat: nest

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule],
 })
 export class AppModule {}
